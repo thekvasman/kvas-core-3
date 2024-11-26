7aa43a38f165ac9ab2d222cd30513fa38a80e97f
Teperikov_Anton <lastsearching31@mail.ru>
2024-11-26 23:10:14 +0300

    правки тестов

--- src/test/ReadWriteFileExceptionHandlingTest.java
+++ src/test/ReadWriteFileExceptionHandlingTest.java
@@ -1,90 +1,115 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-
-import org.junit.Test;
-import task.io.ReadWriteFileExceptionHandling;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import task.io.ReadWriteFileExceptionHandling;
 
 public class ReadWriteFileExceptionHandlingTest {
 
-  @Test
-  public void testCreateAndWriteFile() throws IOException {
+  public static void main(String[] args) {
+    try {
+      testCreateAndWriteFile();
+      testDivideNumbers();
+      testConvertStringToNumber();
+      testCreateFileAndReadFirstWord();
+    } catch (IOException e) {
+      System.out.println("Ошибка ввода-вывода: " + e.getMessage());
+    }
+  }
+
+  public static void testCreateAndWriteFile() throws IOException {
     // given
     String fileName = "test_output.txt";
     String content = "Test content";
-
     // when
     ReadWriteFileExceptionHandling.createAndWriteFile(fileName, content);
     String actualResult = new String(Files.readAllBytes(Paths.get(fileName)));
-
     // then
-    assertEquals(content, actualResult);
-
+    if (content.equals(actualResult)) {
+      System.out.println("Тест 1 (createAndWriteFile) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 1 (createAndWriteFile) не пройден. Ожидался: " + content + ", получен: "
+              + actualResult);
+    }
     // Удаление тестового файла
     Files.deleteIfExists(Paths.get(fileName));
   }
 
-  @Test
-  public void testDivideNumbers() {
+  public static void testDivideNumbers() {
     // given
     double expectedResult = 2.0;
     int numerator = 10;
     int denominator = 5;
-
     // when
     double actualResult = ReadWriteFileExceptionHandling.divideNumbers(numerator, denominator);
-
     // then
-    assertEquals(expectedResult, actualResult, 0.0001);
+    if (expectedResult == actualResult) {
+      System.out.println("Тест 2 (divideNumbers) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 2 (divideNumbers) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
 
     // given
     Integer given = 10;
     Integer givenDenominator = 0;
-
     // when & then
-    assertThrows(ArithmeticException.class,
-        () -> ReadWriteFileExceptionHandling.divideNumbers(given, givenDenominator));
+    try {
+      ReadWriteFileExceptionHandling.divideNumbers(given, givenDenominator);
+      System.out.println(
+          "Тест 2 (divideNumbers) не пройден. Ожидалось исключение ArithmeticException.");
+    } catch (ArithmeticException e) {
+      System.out.println("Тест 2 (divideNumbers) с делением на ноль пройден успешно!");
+    }
   }
 
-  @Test
-  public void testConvertStringToNumber() {
+  public static void testConvertStringToNumber() {
     // given
     int expectedResult = 123;
     String input = "123";
-
     // when
     int actualResult = ReadWriteFileExceptionHandling.convertStringToNumber(input);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult == actualResult) {
+      System.out.println("Тест 3 (convertStringToNumber) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 3 (convertStringToNumber) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
 
     // given
     String given = "abc";
-
     // when & then
-    assertThrows(NumberFormatException.class, () -> ReadWriteFileExceptionHandling.convertStringToNumber(given));
+    try {
+      ReadWriteFileExceptionHandling.convertStringToNumber(given);
+      System.out.println(
+          "Тест 3 (convertStringToNumber) не пройден. Ожидалось исключение NumberFormatException.");
+    } catch (NumberFormatException e) {
+      System.out.println("Тест 3 (convertStringToNumber) с некорректной строкой пройден успешно!");
+    }
   }
 
-  @Test
-  public void testCreateFileAndReadFirstWord() throws IOException {
+  public static void testCreateFileAndReadFirstWord() throws IOException {
     // given
     String fileName = "test_first_word.txt";
     String content = "First Second Third";
     String expectedResult = "First";
-
     // when
     ReadWriteFileExceptionHandling.createFileAndReadFirstWord(fileName, content);
     String fileContent = new String(Files.readAllBytes(Paths.get(fileName)));
     String actualResult = fileContent.split(" ")[0];
-
     // then
-    assertEquals(expectedResult, actualResult);
-
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 4 (createFileAndReadFirstWord) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 4 (createFileAndReadFirstWord) не пройден. Ожидался: " + expectedResult
+              + ", получен: " + actualResult);
+    }
     // Удаление тестового файла
     Files.deleteIfExists(Paths.get(fileName));
   }
--- src/test/StreamTest.java
+++ src/test/StreamTest.java
@@ -1,190 +1,242 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-import task.stream.Stream;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import task.stream.Stream;
 
 public class StreamTest {
 
-  @Test
-  public void testAddNumbers() {
+  public static void main(String[] args) {
+    testAddNumbers();
+    testFilterWordsByLetter();
+    testSquareNumbers();
+    testSumOfNumbers();
+    testAverageOfNumbers();
+    testFindMaxNumber();
+    testFindMinNumber();
+    testContainsValue();
+    testRemoveDuplicates();
+    testStringLengths();
+    testConcatenateStrings();
+    testFilterEvenNumbers();
+    testFindFirstEvenNumber();
+  }
+
+  public static void testAddNumbers() {
     // given
     int num1 = 5;
     int num2 = 10;
     int expectedResult = 15;
-
     // when
     int actualResult = Stream.addNumbers(num1, num2);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult == actualResult) {
+      System.out.println("Тест 1 (addNumbers) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 1 (addNumbers) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testFilterWordsByLetter() {
+  public static void testFilterWordsByLetter() {
     // given
     List<String> words = List.of("apple", "banana", "cherry", "date", "elderberry");
     char letter = 'a';
     List<String> expectedResult = List.of("apple");
-
     // when
     List<String> actualResult = Stream.filterWordsByLetter(words, letter);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 2 (filterWordsByLetter) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 2 (filterWordsByLetter) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testSquareNumbers() {
+  public static void testSquareNumbers() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5);
     List<Integer> expectedResult = List.of(1, 4, 9, 16, 25);
-
     // when
     List<Integer> actualResult = Stream.squareNumbers(numbers);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 3 (squareNumbers) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 3 (squareNumbers) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testSumOfNumbers() {
+  public static void testSumOfNumbers() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5);
     int expectedResult = 15;
-
     // when
     int actualResult = Stream.sumOfNumbers(numbers);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult == actualResult) {
+      System.out.println("Тест 4 (sumOfNumbers) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 4 (sumOfNumbers) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testAverageOfNumbers() {
+  public static void testAverageOfNumbers() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5);
     double expectedResult = 3.0;
-
     // when
     OptionalDouble actualResult = Stream.averageOfNumbers(numbers);
-
     // then
-    assertTrue(actualResult.isPresent());
-    assertEquals(expectedResult, actualResult.getAsDouble(), 0.0001);
+    if (actualResult != null && actualResult.isPresent()
+        && expectedResult == actualResult.getAsDouble()) {
+      System.out.println("Тест 5 (averageOfNumbers) пройден успешно!");
+    } else {
+      if (actualResult == null) {
+        System.out.println("Тест 5 не пройден actualResult: " + actualResult);
+      } else {
+        System.out.println(
+            "Тест 5 (averageOfNumbers) не пройден. Ожидался: " + expectedResult + ", получен: " + (
+                actualResult.isPresent() ? actualResult.getAsDouble() : "не найден"));
+      }
+    }
   }
 
-  @Test
-  public void testFindMaxNumber() {
+  public static void testFindMaxNumber() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5);
     int expectedResult = 5;
-
     // when
     Optional<Integer> actualResult = Stream.findMaxNumber(numbers);
-
     // then
-    assertTrue(actualResult.isPresent());
-    assertEquals(expectedResult, actualResult.get().intValue());
+    if (actualResult != null && actualResult.isPresent() && expectedResult == actualResult.get()) {
+      System.out.println("Тест 6 (findMaxNumber) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 6 (findMaxNumber) не пройден. Ожидался: " + expectedResult + ", получен: " + (
+              actualResult != null && actualResult.isPresent() ? actualResult.get() : "не найден"));
+    }
   }
 
-  @Test
-  public void testFindMinNumber() {
+  public static void testFindMinNumber() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5);
     int expectedResult = 1;
-
     // when
     Optional<Integer> actualResult = Stream.findMinNumber(numbers);
-
     // then
-    assertTrue(actualResult.isPresent());
-    assertEquals(expectedResult, actualResult.get().intValue());
+    if (actualResult != null && actualResult.isPresent() && expectedResult == actualResult.get()) {
+      System.out.println("Тест 7 (findMinNumber) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 7 (findMinNumber) не пройден. Ожидался: " + expectedResult + ", получен: " + (
+              actualResult != null && actualResult.isPresent() ? actualResult.get() : "не найден"));
+    }
   }
 
-  @Test
-  public void testContainsValue() {
+  public static void testContainsValue() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5);
     int value = 3;
     boolean expectedResult = true;
-
     // when
     boolean actualResult = Stream.containsValue(numbers, value);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult == actualResult) {
+      System.out.println("Тест 8 (containsValue) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 8 (containsValue) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testRemoveDuplicates() {
+  public static void testRemoveDuplicates() {
     // given
     List<Integer> numbers = List.of(1, 2, 2, 3, 4, 4, 5);
     List<Integer> expectedResult = List.of(1, 2, 3, 4, 5);
-
     // when
     List<Integer> actualResult = Stream.removeDuplicates(numbers);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 9 (removeDuplicates) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 9 (removeDuplicates) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testStringLengths() {
+  public static void testStringLengths() {
     // given
     List<String> words = List.of("apple", "banana", "cherry", "date", "elderberry");
     List<Integer> expectedResult = List.of(5, 6, 6, 4, 10);
-
     // when
     List<Integer> actualResult = Stream.stringLengths(words);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 10 (stringLengths) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 10 (stringLengths) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testConcatenateStrings() {
+  public static void testConcatenateStrings() {
     // given
     List<String> words = List.of("apple", "banana", "cherry", "date", "elderberry");
     String expectedResult = "apple banana cherry date elderberry";
-
     // when
     String actualResult = Stream.concatenateStrings(words);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 11 (concatenateStrings) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 11 (concatenateStrings) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testFilterEvenNumbers() {
+  public static void testFilterEvenNumbers() {
     // given
     List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
     List<Integer> expectedResult = List.of(2, 4, 6);
-
     // when
     List<Integer> actualResult = Stream.filterEvenNumbers(numbers);
-
     // then
-    assertEquals(expectedResult, actualResult);
+    if (expectedResult.equals(actualResult)) {
+      System.out.println("Тест 12 (filterEvenNumbers) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 12 (filterEvenNumbers) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + actualResult);
+    }
   }
 
-  @Test
-  public void testFindFirstEvenNumber() {
+  public static void testFindFirstEvenNumber() {
     // given
     List<Integer> numbers = List.of(1, 3, 5, 6, 7, 8);
     int expectedResult = 6;
-
     // when
     Optional<Integer> actualResult = Stream.findFirstEvenNumber(numbers);
-
     // then
-    assertTrue(actualResult.isPresent());
-    assertEquals(expectedResult, actualResult.get().intValue());
+    if (actualResult != null && actualResult.isPresent() && expectedResult == actualResult.get()) {
+      System.out.println("Тест 13 (findFirstEvenNumber) пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 13 (findFirstEvenNumber) не пройден. Ожидался: " + expectedResult + ", получен: "
+              + (actualResult != null && actualResult.isPresent() ? actualResult.get()
+              : "не найден"));
+    }
   }
 }
